71520d76b8f835e9a1b7fb51116fb1c9c138a22f
Alícia Stefany <64162281+aliciastefany@users.noreply.github.com>
2026-03-19 18:09:56 +0000

    atualizacao

--- Aula02_SPA/src/App.jsx
+++ Aula02_SPA/src/App.jsx
@@ -1,28 +1,27 @@
 import Home from './pages/Home'
-import Contatos from './pages/Contatos'
 import Sobre from './pages/Sobre'
+import Contatos from './pages/Contatos'
 import { useState } from 'react'
 
 function App(){
-  
   const [pagina, setPagina] = useState('Home')
 
   const renderizarPagina = ()=>{
-    if (pagina === 'Home') return <Home/>
-    if (pagina === 'Contatos') return <Contatos/>
-    if (pagina === 'Sobre') return <Sobre/>
+    if(pagina === 'Home') return <Home/>
+    if(pagina === 'Sobre') return <Sobre/>
+    if(pagina === 'Contatos') return <Contatos/>
   }
-  
+ 
   return(
     <div className="container">
-      <h1>Meu Site SPA</h1>
+      <h1>MEU SITE</h1>
+      <p>Esse é meu primeiro site</p>
       <nav>
         <button onClick={()=>setPagina('Home')}>Home</button>
-        <button onClick={()=>setPagina('Sobre')}>Sobre</button>
         <button onClick={()=>setPagina('Contatos')}>Contatos</button>
+        <button onClick={()=>setPagina('Sobre')}>Sobre</button>
       </nav>
-
-      <div className='conteudo'>
+      <div>
         {renderizarPagina()}
       </div>
     </div>
--- Aula02_SPA/src/pages/Home/index.jsx
+++ Aula02_SPA/src/pages/Home/index.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react"
+
 function Home(){
+    let contador = 0
+    const [contadorState, setContadorState] = useState(0)
+
+    function somar(){
+        contador++
+        console.log(contador)
+    }
+
+    function somarState(){
+        setContadorState(contadorState+1)
+    }
+
     return(
         <div>
             <h1>Página Inicial</h1>
             <p>Essa é a primeira página do site.</p>
+
+            <p>{contador}</p>
+            <button onClick={somar}>Somar</button>
+
+            <p>{contadorState}</p>
+            <button onClick={somarState}>Somar</button>
         </div>
     )
 }
